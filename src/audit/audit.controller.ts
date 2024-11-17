1e7e89283f02f1f742691c7ef79af60970592c74
apsi <apsarasandamali321@gmail.com>
2024-11-17 15:07:28 +0530

    get audit history by flag id & flag value id

--- src/audit/audit.controller.ts
+++ src/audit/audit.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AuditService } from './audit.service';
 
 @Controller('audit')
 export class AuditController {
   constructor(private readonly auditService: AuditService) {}
+
+  @Get('flag/:id')
+  getAuditHistoryPerFlag(@Param('id') flagId: string) {
+    return this.auditService.getAuditHistoryPerFlag(flagId);
+  }
+
+  @Get('flag-value/:id')
+  getAuditHistoryPerFlagValueId(@Param('id') flagValueId: string) {
+    return this.auditService.getAuditHistoryPerFlagValueId(flagValueId);
+  }
 }
