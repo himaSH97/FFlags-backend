c52036c1e071d6b1b09eea37d347e6dd9b3f26d9
himaSH <66290086+himaSH97@users.noreply.github.com>
2024-11-20 12:38:04 +0530

    Revert "get audit history by flag id & flag value id"

--- src/audit/audit.controller.ts
+++ src/audit/audit.controller.ts
@@ -1,17 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AuditService } from './audit.service';
 
 @Controller('audit')
 export class AuditController {
   constructor(private readonly auditService: AuditService) {}
-
-  @Get('flag/:id')
-  getAuditHistoryPerFlag(@Param('id') flagId: string) {
-    return this.auditService.getAuditHistoryPerFlag(flagId);
-  }
-
-  @Get('flag-value/:id')
-  getAuditHistoryPerFlagValueId(@Param('id') flagValueId: string) {
-    return this.auditService.getAuditHistoryPerFlagValueId(flagValueId);
-  }
 }
