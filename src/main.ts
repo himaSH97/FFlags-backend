b3c76bd4405725dc0726807cbf9fff5957e07abc
himash <nipun946@gmail.com>
2024-12-06 12:34:25 +0530

    adding correct port for deployment

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, documentFactory);
 
-  await app.listen(4000);
+  await app.listen(3000);
 }
 bootstrap();
